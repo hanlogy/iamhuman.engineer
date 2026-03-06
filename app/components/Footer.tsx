140a06e0094041ae3f74ae899a3b87430d6364ed
Zhiguang Chen <zhiguang@hanlogy.com>
2026-03-06 13:55:20 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,8 @@
-import { gitHubUrl } from '../definitions/constants';
-import { GitHubSvg } from './svgs';
-
 export function Footer() {
   return (
     <footer className="px-4 py-4 text-center sm:py-8">
       <div className="text-foreground-muted flex-center flex-col text-center text-sm">
         © 2026 IAmHuman.Engineer
-        <a className="mt-2" href={gitHubUrl} target="_blank">
-          <GitHubSvg className="w-6" />
-        </a>
       </div>
     </footer>
   );
