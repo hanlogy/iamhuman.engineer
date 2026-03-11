200566c3f9b6a46775f81f4330c5126e5c56323e
Zhiguang Chen <zhiguang@hanlogy.com>
2026-03-11 07:50:20 +0100

    Fix login

--- app/actions/user/login.ts
+++ app/actions/user/login.ts
@@ -52,7 +52,7 @@ export async function login({
     }
 
     const { setSession } = await createSessionManager();
-    setSession({
+    await setSession({
       accessToken,
       refreshToken,
       expiresIn,
