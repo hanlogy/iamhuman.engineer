5bcc20ff61a696b904a2a8d912144d643a7789f8
Zhiguang Chen <zhiguang@hanlogy.com>
2026-03-14 08:09:29 +0100

    Finish change passowrd

---
+++ app/actions/settings/changePassword.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import {
+  InvalidPasswordException,
+  LimitExceededException,
+  NotAuthorizedException,
+} from '@aws-sdk/client-cognito-identity-provider';
+import {
+  toActionFailure,
+  toActionSuccess,
+  type ActionResponse,
+  type ErrorCode,
+} from '@hanlogy/react-kit';
+import { createSessionManager } from '@/server/auth';
+import { getCognitoHelper } from '@/server/helpersRepo';
+
+export async function changePassword({
+  oldPassword,
+  newPassword,
+}: {
+  oldPassword: string;
+  newPassword: string;
+}): Promise<ActionResponse> {
+  const { getSession } = await createSessionManager();
+
+  const session = await getSession();
+  if (!session) {
+    return toActionFailure();
+  }
+
+  const cognitoHelper = getCognitoHelper();
+
+  try {
+    await cognitoHelper.changePassword({
+      previousPassword: oldPassword,
+      proposedPassword: newPassword,
+      accessToken: session.accessToken,
+    });
+
+    return toActionSuccess();
+  } catch (error) {
+    let code: ErrorCode = 'unknown';
+    if (error instanceof NotAuthorizedException) {
+      code = 'notAuthorized';
+    } else if (error instanceof InvalidPasswordException) {
+      code = 'invalidPassword';
+    } else if (error instanceof LimitExceededException) {
+      code = 'limitExceeded';
+    }
+    return toActionFailure({ code });
+  }
+}
--- app/settings/password/PasswordForm.tsx
+++ app/settings/password/PasswordForm.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { useState, type SubmitEvent } from 'react';
 import { useForm } from '@hanlogy/react-web-ui';
+import { changePassword } from '@/actions/settings/changePassword';
 import { FilledButton } from '@/components/buttons/FilledButton';
+import { FormErrorMessage } from '@/components/form/FormErrorMessage';
 import { PasswordField } from '@/components/form/fields';
 
 interface FormData {
@@ -11,31 +14,120 @@ interface FormData {
 }
 
 export function PasswordForm() {
-  const { register } = useForm<FormData>();
+  const {
+    register,
+    validate,
+    getValues,
+    setFieldError,
+    setFormError,
+    setFormErrorListener,
+  } = useForm<FormData>();
+
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  const handleSubmit = async (e: SubmitEvent) => {
+    e.preventDefault();
+    if (!validate()) {
+      return;
+    }
+
+    setIsPending(true);
+
+    const { newPassword, oldPassword } = getValues();
+    if (!newPassword || !oldPassword) {
+      return;
+    }
+
+    const saveResult = await changePassword({
+      newPassword,
+      oldPassword,
+    });
+
+    setIsPending(false);
+
+    if (saveResult.error) {
+      switch (saveResult.error.code) {
+        case 'notAuthorized':
+          setFieldError('oldPassword', 'Current password is wrong');
+          return;
+        case 'invalidPassword':
+          setFieldError('newPassword', 'New password is invalid');
+          return;
+
+        case 'limitExceeded':
+          setFormError('Attempt limit exceeded, please try after some time');
+          return;
+      }
+
+      setFormError('failed to change password');
+
+      return;
+    }
+
+    setIsSuccess(true);
+  };
+
+  if (isSuccess) {
+    return (
+      <div className="py-12 text-center">
+        <div className="font-semibold">
+          Your password was changed successfully.
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
-      <form className="space-y-6">
+      <form onSubmit={handleSubmit} className="space-y-6">
         <PasswordField
           label="Current password"
-          controller={register('oldPassword')}
+          controller={register('oldPassword', {
+            validator: ({ oldPassword }) => {
+              if (!oldPassword) {
+                return 'Enter your current password';
+              }
+            },
+          })}
         />
 
         <PasswordField
           label="New password"
-          controller={register('newPassword')}
+          controller={register('newPassword', {
+            validator: ({ newPassword }) => {
+              if (!newPassword) {
+                return 'Enter your new password';
+              }
+            },
+          })}
         />
         <PasswordField
           label="Confirm new password"
-          controller={register('confirmNewPassword')}
+          controller={register('confirmNewPassword', {
+            validator: ({ newPassword, confirmNewPassword }) => {
+              if (!confirmNewPassword) {
+                return 'Confirm your new password';
+              }
+              if (newPassword && newPassword !== confirmNewPassword) {
+                return 'Confirm password does not match';
+              }
+            },
+          })}
         />
 
         <div className="py-5 text-center">
-          <FilledButton className="min-w-40" size="small">
+          <FilledButton
+            disabled={isPending}
+            type="submit"
+            className="min-w-40"
+            size="small"
+          >
             Save
           </FilledButton>
         </div>
       </form>
+      <FormErrorMessage setListener={setFormErrorListener} />
     </>
   );
 }
